67b9adc7db24644eeb6decd77e200eb18453934d
dancheg97 <dangdancheg@gmail.com>
2022-12-12 19:42:09 +0300

    added font setup to vscode

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 # packages
 
 sudo pacman -Syu
-sudo pacman -Sy flatpak nodejs npm steam snapd docker gamemode wireguard-tools gksu go appimagelauncher python-pip make chromium
+sudo pacman -Sy flatpak nodejs npm steam snapd docker gamemode wireguard-tools gksu go appimagelauncher python-pip make chromium ttf-meslo-nerd-font-powerlevel10k
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo pacman -Sl multilib
 
