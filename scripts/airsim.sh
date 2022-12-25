89134cceefd7a4deb8c1d91cd541a6017dd26fd2
dancheg97 <dangdancheg@gmail.com>
2022-12-26 00:07:17 +0300

    asd\

--- scripts/airsim.sh
+++ scripts/airsim.sh
@@ -1,6 +1,3 @@
-pip install msgpack-rpc-python
-pip install airsim
-
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -9,4 +6,13 @@ cd ..
 sudo rm -r yay
 
 yay -Sy unreal-engine
+
+conda install -c conda-forge boost clang=8.0.1 libcxx=8.0.1
+git clone https://github.com/Microsoft/AirSim.git
+cd AirSim
+sed -i '46,53 s/^/#/' setup.sh
+sed -i '63 s/^/#/' setup.sh
+sed -i '97 s/^/#/' setup.sh
+./setup.sh
+./build.sh
 
