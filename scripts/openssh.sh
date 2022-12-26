63e1b0c4e654b5d4c338366cf22daad791656bc3
dancheg97 <dangdancheg@gmail.com>
2022-12-27 02:37:13 +0300

    .

--- scripts/openssh.sh
+++ scripts/openssh.sh
@@ -2,7 +2,9 @@ sudo pacman -Sy openssh
 sudo systemctl status sshd.service
 
 echo 'Host *
-  PasswordAuthentication no' | xclip -selection clipboard
+  PasswordAuthentication no
+  AuthenticationMethods publickey' | xclip -selection clipboard
+sudo nano /etc/ssh/sshd_config
 
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
