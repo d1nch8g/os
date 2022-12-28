d6454adfdf5bba7b3a55c008851b6d335b94a67a
dancheg97 <dangdancheg@gmail.com>
2022-12-28 12:17:00 +0300

    added godot to default installed packages

--- arch/packages.sh
+++ arch/packages.sh
@@ -1,4 +1,4 @@
 sudo pacman -Syu
-sudo pacman -Sy flatpak nodejs npm steam snapd docker gamemode wireguard-tools gksu go appimagelauncher python-pip make chromium ttf-meslo-nerd-font-powerlevel10k telegram-desktop mattermost-desktop discord
+sudo pacman -Sy flatpak nodejs npm steam snapd docker gamemode wireguard-tools gksu go appimagelauncher python-pip make chromium ttf-meslo-nerd-font-powerlevel10k telegram-desktop mattermost-desktop discord godot
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo pacman -Sl multilib
