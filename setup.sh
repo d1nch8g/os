9dd485551b5b035d3746597e9c51733f9a04d658
USERNAME <EMAIL>
2023-04-16 11:24:09 +0700

    added go tooling installation process

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ cp ~/os/drone.desktop ~/.local/share/applications/drone.desktop
 
 go install github.com/ktr0731/evans@latest
 go install mvdan.cc/gofumpt@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/fatih/gomodifytags@latest
+go install github.com/josharian/impl@latest
+go install github.com/haya14busa/goplay/cmd/goplay@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+go install golang.org/x/tools/gopls@latest
 
 systemctl start docker.service
 systemctl enable docker.service
