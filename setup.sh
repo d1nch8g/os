7dd03334533e67f1f557bace4fc4326f71e4b959
dancheg <dancheg@fmnx.su>
2023-05-24 10:22:58 +0700

    configuration changes and extension installation enhanced

--- setup.sh
+++ setup.sh
@@ -10,17 +10,17 @@ go install github.com/haya14busa/goplay/cmd/goplay@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 go install golang.org/x/tools/gopls@latest
-code --install-extension esbenp.prettier-vscode &
-code --install-extension ms-azuretools.vscode-docker &
-code --install-extension PKief.material-icon-theme &
-code --install-extension ms-vscode-remote.remote-ssh &
-code --install-extension redhat.vscode-yaml &
-code --install-extension golang.Go &
-code --install-extension mhutchie.git-graph &
-code --install-extension rangav.vscode-thunder-client &
-code --install-extension zxh404.vscode-proto3 &
-code --install-extension Dart-Code.flutter &
-code --install-extension IronGeek.vscode-env &
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension PKief.material-icon-theme
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension redhat.vscode-yaml
+code --install-extension golang.Go
+code --install-extension mhutchie.git-graph
+code --install-extension rangav.vscode-thunder-client
+code --install-extension zxh404.vscode-proto3
+code --install-extension Dart-Code.flutter
+code --install-extension IronGeek.vscode-env
 code --install-extension jeanp413/open-remote-ssh
 cd ~
 sudo rm -rf Pictures
