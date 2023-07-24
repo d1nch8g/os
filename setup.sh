a01f67bee6364b96004d40fd501b8e30b2e43a9b
Danila Fominykh <dancheg97@fmnx.su>
2023-07-24 15:38:14 +0300

    articles, changes extension set

--- setup.sh
+++ setup.sh
@@ -26,19 +26,15 @@ go install golang.org/x/tools/gopls@latest
 
 # Setting up VSCodium.
 alias code=vscodium
+code --install-extension PKief.material-icon-theme
 code --install-extension esbenp.prettier-vscode
 code --install-extension ms-azuretools.vscode-docker
-code --install-extension PKief.material-icon-theme
-code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension redhat.vscode-yaml
+code --install-extension IronGeek.vscode-env
 code --install-extension golang.Go
 code --install-extension mhutchie.git-graph
-code --install-extension rangav.vscode-thunder-client
-code --install-extension zxh404.vscode-proto3
 code --install-extension Dart-Code.flutter
-code --install-extension IronGeek.vscode-env
 code --install-extension jeanp413.open-remote-ssh
-code --install-extension Dancheg97.grpc-clicker
 code --install-extension jeff-hykin.mario
 cp ~/os/settings.json ~/.config/VSCodium/User/settings.json
 cp ~/os/keybindings.json ~/.config/VSCodium/User/keybindings.json
@@ -49,6 +45,7 @@ git clone https://fmnx.su/core/iso
 git clone https://fmnx.su/core/pack
 git clone https://fmnx.su/core/infr
 git clone https://fmnx.su/core/ainst
+git clone https://fmnx.su/dancheg97/articles
 cd ~
 
 # Set up vmware
