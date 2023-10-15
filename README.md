d8826b8192c4cdd793e6d81e907e571c6e7d3ecb
Danila Fominykh <d1nch8g@ion.lc>
2023-10-15 12:15:07 -0300

    split repositories and system setup

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 <img style="align: center; padding-left: 10px; padding-right: 10px; padding-bottom: 10px;" width="238px" height="238px" src="./logo.png" />
 </p>
 
-<h2 align="center">Dancheg97 os setup script</h2>
+<h2 align="center">d1nch8g os setup script</h2>
 
 Get all the environment prepared for full-stack development in a single command right after installation.
 
 ```sh
-cd ~ && git clone https://fmnx.su/dancheg97/os && cd os && sh setup.sh
+cd ~ && git clone https://ion.lc/d1nch8g/os && cd os && sh setup.sh
 ```
 
 This development stack contains:
@@ -19,3 +19,7 @@ This development stack contains:
 5. Main FMNX dev repos.
 
 All this tools will be installed in a single command, just run it once after a fresh installation.
+
+```sh
+cd ~ && git clone https://ion.lc/d1nch8g/os && cd os && sh repos.sh
+```
---
+++ repos.sh
@@ -0,0 +1,74 @@
+# Cloning all my repositories working repos.
+
+cd ~
+git clone https://ion.lc/d1nch8g/os
+git clone https://ion.lc/d1nch8g/iso
+git clone https://ion.lc/d1nch8g/ainst
+git clone https://ion.lc/d1nch8g/pack
+git clone https://ion.lc/d1nch8g/pacman
+git clone https://ion.lc/d1nch8g/help
+git clone https://ion.lc/d1nch8g/articles
+git clone https://ion.lc/d1nch8g/grpc-clicker
+
+cd ~
+mkdir ion_archive
+git clone https://ion.lc/d1nch8g/timemocks
+git clone https://ion.lc/d1nch8g/pkg-tests
+git clone https://ion.lc/d1nch8g/package
+git clone https://ion.lc/d1nch8g/bigflag
+git clone https://ion.lc/d1nch8g/repo
+git clone https://ion.lc/d1nch8g/registry
+git clone https://ion.lc/d1nch8g/go-reflect
+git clone https://ion.lc/d1nch8g/testrepo
+
+cd ~
+mkdir github_archive
+cd github_archive
+git clone https://github.com/d1nch8g/flow-case
+git clone https://github.com/d1nch8g/learn_typescript
+git clone https://github.com/d1nch8g/learn_ts
+git clone https://github.com/d1nch8g/grpcclicker_flutter
+git clone https://github.com/d1nch8g/grpc_clicker
+git clone https://github.com/d1nch8g/learnjs
+git clone https://github.com/d1nch8g/gateway
+git clone https://github.com/d1nch8g/learn_vsc_extensions
+git clone https://github.com/d1nch8g/learn_svelte
+git clone https://github.com/d1nch8g/learncss
+git clone https://github.com/d1nch8g/vscode-material-icon-theme
+git clone https://github.com/d1nch8g/discord_alerts
+git clone https://github.com/d1nch8g/goenv
+git clone https://github.com/d1nch8g/go_udp_server_example
+git clone https://github.com/d1nch8g/assets
+git clone https://github.com/d1nch8g/tradebits
+git clone https://github.com/d1nch8g/orb_cloud_mobile
+git clone https://github.com/d1nch8g/godotenv
+git clone https://github.com/d1nch8g/softdigger
+git clone https://github.com/d1nch8g/integer_conversion_test
+git clone https://github.com/d1nch8g/nw_auc_checker
+git clone https://github.com/d1nch8g/sync_tree_mobile
+git clone https://github.com/d1nch8g/sync_tree_mobile_logic
+git clone https://github.com/d1nch8g/crypto_export
+git clone https://github.com/d1nch8g/gitea
+git clone https://github.com/d1nch8g/infr
+git clone https://github.com/d1nch8g/iso
+git clone https://github.com/d1nch8g/ainst
+git clone https://github.com/d1nch8g/go-flags
+git clone https://github.com/d1nch8g/grpc-clicker
+git clone https://github.com/d1nch8g/coin-dom
+git clone https://github.com/d1nch8g/package
+git clone https://github.com/d1nch8g/pack
+git clone https://github.com/d1nch8g/pacser
+git clone https://github.com/d1nch8g/pacman
+git clone https://github.com/d1nch8g/repo
+git clone https://github.com/d1nch8g/ga-hugo-theme
+git clone https://github.com/d1nch8g/setup
+git clone https://github.com/d1nch8g/go-pacman
+git clone https://github.com/d1nch8g/gen-tools
+git clone https://github.com/d1nch8g/gitea_setup
+git clone https://github.com/d1nch8g/linux_yandex_music
+git clone https://github.com/d1nch8g/clean_go_svc
+git clone https://github.com/d1nch8g/oapi-codegen
+git clone https://github.com/d1nch8g/gRPC_api_example
+git clone https://github.com/d1nch8g/test_grpc_call_page
+git clone https://github.com/d1nch8g/svelte_new_project_tests
+git clone https://github.com/d1nch8g/flow-case
