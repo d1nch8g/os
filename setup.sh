902e02956200738a42f3b405668b1c024bca0091
Danila Fominykh <d1nch8g@gmail.com>
2024-12-05 00:16:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sed -i 's|robbyrussell|powerlevel10k/powerlevel10k|g' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-
+echo "newgrp docker" >> ~/.zshrc
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
