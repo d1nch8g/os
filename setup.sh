000f4abc6c8babc42075a661a430d26bc1809cbc
Danila Fominykh <d1nch8g@gmail.com>
2024-12-07 13:02:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,4 +61,6 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
 
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+
 p10k configure
